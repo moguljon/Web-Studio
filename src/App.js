3e11ee8862982a4e35b32c648cc141b078d30d47
unknown <jonathanaguilar@moguljon.com>
2020-12-12 23:06:00 -0500

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import Navigation from './components/Navigation/Navigation';
 import Header from './components/Header/Header';
@@ -23,8 +23,17 @@ import {Helmet} from "react-helmet";
 import Endrose from './components/Endorse/Endorse';
 import Moguljon from './components/Moguljon/moguljon';
 import Courses from './components/Courses/Courses';
+import ReactGa from 'react-ga';
 
 function App() {
+
+  useEffect(() => {
+    ReactGa.initialize('G-Q3EQSE5TGD')
+
+    //To report page view 
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
+
     return (
         <div>
         <Helmet>
