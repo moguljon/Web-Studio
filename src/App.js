25bc7cd090a0183635ed9d46f8479d39628ac704
unknown <jonathanaguilar@moguljon.com>
2020-12-21 01:29:32 -0500

    added a 404 component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import './App.scss';
 import Navigation from './components/Navigation/Navigation';
-import Header from './components/Header/Header';
-import About from './components/About/About';
-import Feature from './components/Feature/Feature';
-import Prices from './components/Prices/Prices';
-import Story from './components/Story/Story';
 import Booking from './components/Booking/Booking';
 import Footer from './components/Footer/Footer';
 import Blog from './components/Blog/Blog';
 import ScrollToTop from './components/ScrollToTop';
 import Work from './components/Work/Work';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import 'tachyons';
 import Newsletter from './components/Newsletter/Newsletter';
 import Social from  './components/Social/Social';
 import {Helmet} from "react-helmet";
-import Endrose from './components/Endorse/Endorse';
 import Moguljon from './components/Moguljon/moguljon';
-
+import Nomatch from './components/Nomatch/Nomatch';
+import Home from './components/Home/Home';
 
 function App() {
     return (
@@ -30,18 +25,15 @@ function App() {
         <Router>
         <ScrollToTop />
         <Navigation />
-          <Route exact path='/' component={Header} /> 
-          <Route exact path='/' component={Endrose} /> 
-          <Route exact path='/' component={About} />
-          <Route exact path='/' component={Feature} />
-          <Route exact path='/' component={Prices} />
-          <Route exact path='/' component={Story} />
-          <Route exact path='/' component={Work} /> 
+       <Switch>
+          <Route exact path='/' component={Home} />
           <Route exact path='/newsletter' component={Newsletter} />  
           <Route exact path='/booking' component={Booking} />
           <Route exact path='/work' component={Work} />
           <Route exact path='/blog' component={Blog} />
           <Route exact path='/portfolio' component={Moguljon} />
+          <Route component={Nomatch} />
+        </Switch>
           <Social />
           <Footer />
         </Router>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Endrose from '../Endorse/Endorse';
+import About from '../About/About';
+import Feature from '../Feature/Feature';
+import Prices from '../Prices/Prices';
+import Story from '../Story/Story';
+import Work from '../Work/Work';
+
+const Home = () => {
+    return(
+        <div>
+            <Header />
+            <Endrose />
+            <About />
+            <Feature />
+            <Prices />
+            <Story />
+            <Work />
+        </div>
+    )
+}
+
+export default Home;
