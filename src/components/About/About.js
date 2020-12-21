886e207ee62e085eecf86c4c80911994da3d6a19
unknown <jonathanaguilar@moguljon.com>
2020-12-21 00:06:43 -0500

    added a different feature section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import './About.scss';
-import { Link } from 'react-router-dom';
-import illustration from './img/canva.png';
-import canva1 from './img/canva1.jpg';
-import canva3 from './img/canva3.png';
+// import { Link } from 'react-router-dom';
+// import illustration from './img/canva.png';
+// import canva1 from './img/canva1.jpg';
+// import canva3 from './img/canva3.png';
+// import { CgAlbum } from "react-icons/cg";
 
 
 const About = () => {
   return(
       <section className='section-about'>
+         <div className='u-center-text u-margin-bottom-big'>
+          <h2 className='heading-secondary'>What We Do</h2>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-3'>
+            <div className='section-about__container'>
+              <ion-icon name="albums-outline" className='icon1' size='large'  ></ion-icon>
+              <h2 className='section-about__header--two'>UI/UX</h2>
+              <p className='section-about__paragraph'>Landing Pages, User Flow, Wireframing, Prototyping, Mobile App Design, Web App</p>
+            </div>
+          </div>
+          <div className='col-1-of-3'>
+            <div className='section-about__container'>
+              <ion-icon name="bulb-outline" className='section-about__color' size='large'  ></ion-icon>
+              <h2 className='section-about__header--two'>Branding</h2>
+              <p className='section-about__paragraph'>Character Design, Icon Set, Illustration Guide, Illustration Set</p>
+            </div>
+          </div>
+          <div className='col-1-of-3'>
+            <div className='section-about__container'>
+              <ion-icon name="bag-check-outline" size='large'  ></ion-icon>
+              <h2 className='section-about__header--two'>Development</h2>
+              <p className='section-about__paragraph'>HTML/CSS, JavaScript Animation, Shopify, Responsive Website</p>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+        
+        {/* 
+        
         <div className='u-center-text u-margin-bottom-big'>
           <h2 className='heading-secondary'>What We Do</h2>
         </div>
@@ -32,7 +68,7 @@ const About = () => {
             <img src={canva3} alt='image3' className='composition__photo composition__photo--p3' />
            </div>
           </div>
-        </div>
+        </div> */}
       </section>
   )
 }
