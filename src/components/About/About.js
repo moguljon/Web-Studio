875b71c9967049156a2499f5834f239ed4ae5d21
unknown <jonathanaguilar@moguljon.com>
2020-12-21 22:47:18 -0500

    adding features on about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,18 +14,32 @@ const About = () => {
           <h2 className='heading-secondary'>What We Do</h2>
         </div>
         <div className='row2'>
-          <div className='col-1-of-2'>
+          <div className='col-1-of-4'>
             <div className='section-about__container'>
               <ion-icon name="albums-outline" className='icon1' size='large'  ></ion-icon>
               <h2 className='section-about__header--two'>Design</h2>
-              <p className='section-about__paragraph'>We create digital assets that for your business needs. </p>
+              <p className='section-about__paragraph'>We create digital assets that fullfils your business needs. </p>
             </div>
           </div>
-          <div className='col-1-of-2'>
+          <div className='col-1-of-4'>
             <div className='section-about__container'>
               <ion-icon name="bulb-outline" className='section-about__color' size='large'  ></ion-icon>
               <h2 className='section-about__header--two'>Development</h2>
-              <p className='section-about__paragraph'>Character Design, Icon Set, Illustration Guide, Illustration Set</p>
+              <p className='section-about__paragraph'>We design custom code using the latest tools and frameworks to make your project come to life.</p>
+            </div>
+          </div>
+          <div className='col-1-of-4'>
+            <div className='section-about__container'>
+              <ion-icon name="pricetags-outline" className='section-about__color' size='large'  ></ion-icon>
+              <h2 className='section-about__header--two'>Platforms</h2>
+              <p className='section-about__paragraph'>We are able to use a variety of platforms like Squarespace and Shopify .</p>
+            </div>
+          </div>
+          <div className='col-1-of-4'>
+            <div className='section-about__container'>
+              <ion-icon name="globe-outline" className='section-about__color' size='large'  ></ion-icon>
+              <h2 className='section-about__header--two'>Scalibility</h2>
+              <p className='section-about__paragraph'>We create projects that scale as your business grows.</p>
             </div>
           </div>
         </div>
