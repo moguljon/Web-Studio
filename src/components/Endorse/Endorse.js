163097f5fb8461445069e6c7e3b3ca2e94e259f9
unknown <jonathanaguilar@moguljon.com>
2021-02-13 03:29:01 -0500

    more padding on endorse

--- src/components/Endorse/Endorse.js
+++ src/components/Endorse/Endorse.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './Endorse.scss';
 import image1 from './img/shopify2.png';
-import image2 from './img/squarespace.svg';
+import image2 from './img/squarespace2.svg';
 import image3 from './img/wix.svg';
 
 const Endorse = () => { 
   return(
     <section className='section-endorse tc'>
       <p className='section-endorse__paragraph'>We can use modern E-commerce solutions.</p>
-      <div>
+      <div className='section-endorse__container'>
        
         
         
           <img src={image1} alt='' className='section-endorse__image' />
         
 
-          <img  src={image2} alt='' className='section-endorse__image' />
+          <img  src={image2} alt='' className='section-endorse__image resize2' />
      
         
           <img  src={image3} alt='' className='section-endorse__image' />
