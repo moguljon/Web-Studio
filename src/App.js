cef21b8d8c1b87a904173a68f6546c43701cd763
unknown <jonathanaguilar@moguljon.com>
2021-03-29 22:08:03 -0400

    added a loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.scss';
 import Navigation from './components/Navigation/Navigation';
 import Booking from './components/Booking/Booking';
 import Footer from './components/Footer/Footer';
-// import Blog from './components/Blog/Blog';
 import ScrollToTop from './components/ScrollToTop';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import 'tachyons';
@@ -17,33 +16,60 @@ import Terms from './components/Terms/Terms';
 import Cookies from './components/Cookies/Cookies';
 import Resources from './components/Resources/Resources';
 import Bottom from './components/Bottom/Bottom';
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import logo from '../src/img/noun-o.svg';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500)
+  }, []);
   
     return (
         <div>
-          <Helmet>
-            <title>Web Studios | Let's Build Your Brand</title>
-            <meta name="description" content='We leverage technology to create brands from inception to production because we understand that it is much easier to have a guiding hand that understands the technical details of creating a brand.' />
-          </Helmet>
-          <Router>
-            <ScrollToTop />
-            <Navigation />  
-            <Switch>             
-                <Route exact path='/' component={Home} />
-                <Route exact path='/booking' component={Booking} />
-                {/* <Route exact path='/blog' component={Blog} /> */}
-                <Route exact path='/portfolio' component={Moguljon} />
-                <Route exact path='/privacy' component={Privacy} />
-                <Route exact path='/Services' component={Prices} />
-                <Route exact path='/terms' component={Terms} />
-                <Route exact path='/cookies-policy' component={Cookies} />
-                <Route exact path='/resources' component={Resources} />
-                <Route component={Nomatch} />
-            </Switch>
-            <Bottom />
-            <Footer />
-          </Router>
+          
+          {
+         
+            loading ? 
+            
+            <div className='app'>
+              <div className='img2'>
+                <img src={logo} alt='brand representation' />
+              </div>
+                <ClimbingBoxLoader color={'#8098ee'} loading={loading} size={40} />
+            </div>
+            :
+            (
+              <div>
+                <Helmet>
+                  <title>Web Studios | Let's Build Your Brand</title>
+                  <meta name="description" content='We leverage technology to create brands from inception to production because we understand that it is much easier to have a guiding hand that understands the technical details of creating a brand.' />
+                </Helmet>
+                <Router>
+                  <ScrollToTop />
+                  <Navigation />  
+                  <Switch>             
+                      <Route exact path='/' component={Home} />
+                      <Route exact path='/booking' component={Booking} />
+                      <Route exact path='/portfolio' component={Moguljon} />
+                      <Route exact path='/privacy' component={Privacy} />
+                      <Route exact path='/Services' component={Prices} />
+                      <Route exact path='/terms' component={Terms} />
+                      <Route exact path='/cookies-policy' component={Cookies} />
+                      <Route exact path='/resources' component={Resources} />
+                      <Route component={Nomatch} />
+                  </Switch>
+                  <Bottom />
+                  <Footer />
+                </Router>
+              </div>
+            )
+          }
+       
         </div>
     );
   } 
