0615f7ccb2bea3563e7c434d574e1894608c1e22
unknown <jonathanaguilar@moguljon.com>
2021-04-06 17:24:06 -0400

    added new price table - all new design

--- src/components/Prices/Prices.js
+++ src/components/Prices/Prices.js
@@ -4,69 +4,85 @@ import { Link } from 'react-router-dom';
 import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 import {Helmet} from "react-helmet"; 
 import Logo2 from '../Logo2/Logo2';
+import img1 from '../../img/adventure.svg';
+import img2 from '../../img/premium.svg';
+import img3 from '../../img/outerspace.svg';
 
 const Prices = () => {
   return(
     <section className='section-tours'>
       <Logo2 />
       <div className='u-center-text u-margin-bottom-big'>
-        <h2 className='heading-secondary'>Services</h2>
+        <h2 className='heading-secondary'>Simple Pricing Options</h2>
+        <p className='paragraph-heading'>Choose the plan that is right for your business. All plans include a free consultation to explore options.</p>
       </div>
       <Helmet>
          <title>Find the Right Service for You | Web Studios</title>
          <meta name="description" content='Various service packages that Web Studios provides at a reasonable price.' />
       </Helmet>
-      {/* <div className='header__logo-box'>
-        <Link to='/' className='mobile-logo'>
-          <img src={ws} alt='alt' className='header__logo'/>
-        </Link>
-      </div>  */}
+
       <div className='row'>
-        <div className='col-1-of-3 single-price'>
-          <h1  className='price-header-1'>Investment</h1>
-          <div className='price'>
-            <h2 className='price'>Only $297</h2>
+        <div className='col-1-of-3'>
+          <div className='prices'>
+            <div className='prices__container container1'>
+             <img src={img1} className='prices__img' alt="Explore plan"/>
+              <p className='prices__paragraph1'>Explore Plan</p>
+              <h3 className='prices__header-three'>$260</h3>
+            </div>
+            <div className='prices__other-container'>
+              <div className='prices__content'>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>Custom coded website or platform</p>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>3 page layout website</p>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>Responsive design for mobile</p>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>1 round of revision website</p>
+              </div>
+            </div>
+            <div className='prices__button'>
+              <Link to="/booking" className='btn btn--main'>Get Started</Link>
+            </div>
           </div>
-          <div className='deals'>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> Custom website using the latest designs</p>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> Single page layout</p>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> Responsive design</p>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> 1 round of revisions</p>
-          </div>
-          <Link to='/booking' className='btn btn-outline'>Schedule</Link>
         </div>
-   
-        <div className='col-1-of-3 single-price main'>
-          {/* <p className='most-popular'>Most Popular</p> */}
-          <h1  className='price-header-1'>Premium</h1>
-          <div className='price'>
-            <h2 className='price'>Only $495</h2>
+        <div className='col-1-of-3'>
+          <div className='prices'>
+            <div className='prices__container container2'>
+             <img src={img2} className='prices__img1' alt="premium plan"/>
+              <p className='prices__paragraph1'>Premium Plan</p>
+              <h3 className='prices__header-three'>$499</h3>
+            </div>
+            <div className='prices__other-container'>
+              <div className='prices__content'>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>Custom coded website or platform</p>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>5 Page Responsive website</p>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>Up to 5 revisions</p>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>Social media integration</p>
+              </div>
+            </div>
+            <div className='prices__button'>
+              <Link to="/booking" className='btn btn--orange'>Get Started</Link>
+            </div>
           </div>
-          <div className='deals'>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> 3 Page Responsive Website</p>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> Custom Logo</p>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> Brand colors, fonts, and message</p>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> Your choice of platform or custom</p>
-          </div>
-          <Link to='/booking' className='btn btn--main' style={{marginTop: '2rem'}}>Schedule</Link>
         </div>
-        <div className='col-1-of-3 single-price'>
-          <h1  className='price-header-1'>Immersive</h1>
-          <div className='price'>
-            <h2 className='price'>Only $997</h2>
+        <div className='col-1-of-3'>
+          <div className='prices'>
+            <div className='prices__container container3'>
+             <img src={img3} className='prices__img3' alt="Immersive plan"/>
+              <p className='prices__paragraph1'>Immersive Plan</p>
+              <h3 className='prices__header-three'>$699</h3>
+            </div>
+            <div className='prices__other-container'>
+              <div className='prices__content'>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>Unlimited pages</p>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>Unlimited revisions</p>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>Social media campaigns</p>
+                <p className='prices__paragraph'><IoIosCheckmarkCircleOutline className='icon' size={15}/>Brand message, colors, and fonts</p>
+              </div>
+            </div>
+            <div className='prices__button'>
+              <Link to="/booking" className='btn btn--green'>Get Started</Link>
+            </div>
           </div>
-          <div className='deals'>
-            <p>
-              <IoIosCheckmarkCircleOutline size={15} className='icons' /> Custom Shopify website or other
-            </p>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> Shop availability</p>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> Marketing campaigns</p>
-            <p><IoIosCheckmarkCircleOutline size={15} className='icons' /> Social media integration</p>
-          </div>
-          <Link to='/booking' className='btn btn-outline'>Schedule</Link>
         </div>
       </div>
-      
     </section>
   )
 }
